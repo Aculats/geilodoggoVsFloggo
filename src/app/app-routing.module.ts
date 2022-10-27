aa12eb7a99bbb6db585a5f1bf67ab99aaf2a3a85
jan.schultheiss1996@gmail.com <jan.schultheiss1996@gmail.com>
2022-10-27 20:54:18 +0200

    add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'home', component: HomeComponent
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
-  },
-  { 
-    path: 'newsignup',
-    loadChildren: () => import('./newsignup/newsignup.module').then( m => m.NewSignUpModule)
-  },
-  {
-    path: 'listregistrations',
-    loadChildren: () => import('./listRegistrations/listRegistrations.module').then( m => m.ListRegistrationsModule)
-  },
-  {
-    path: 'hunting',
-    loadChildren: () => import('./smileyhunting/smileyhunting.module').then( m => m.SmileyHuntingModule)
   },
 ];
 
