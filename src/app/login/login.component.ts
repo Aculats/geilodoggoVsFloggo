073f087e1df13c10b0d31cfa552a38f58092a90a
jan.schultheiss1996@gmail.com <jan.schultheiss1996@gmail.com>
2022-10-28 07:33:05 +0200

    add start menu

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  public version: string = "0.0.1";
 
   constructor() { }
 
